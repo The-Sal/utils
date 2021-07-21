71e252d66372c93dcced4a5d73e3ca338d7dacee
Sal <salmanfaris2005@hotmail.com>
2021-07-21 21:44:55 +0400

    cbaa

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
 import setuptools
+import utils
 setuptools.setup(
      name='utils',
-     version='1.1',
+     version=utils.version,
      author="Sal Faris",
      description="Utility functions",
      packages=setuptools.find_packages()
