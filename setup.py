4f5ca230a0bc139807ef07580142cc817442cba1
Sal <salmanfaris2005@hotmail.com>
2021-09-08 10:20:30 +0400

    cbaa

--- setup.py
+++ setup.py
@@ -11,6 +11,6 @@ setuptools.setup(
     license='MIT',
     author_email='salmanfaris2005@hotmail.com',
     url='https://github.com/The-Sal/utils/',
-    download_url='https://github.com/The-Sal/utils/archive/refs/tags/v1.6.5.tar.gz',
+    download_url='https://github.com/The-Sal/utils/archive/refs/tags/v{}.tar.gz'.format(version),
     install_requires=reqs
 )
