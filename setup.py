c5b0411827c24f3438a9ec1ab60e1d58f7cb3803
Sal <salmanfaris2005@hotmail.com>
2021-09-23 11:58:26 +0400

    cbaa

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import setuptools
-from utils import version
 reqs = ['requests>=2.26.0',
-        'setuptools>=57.0.0']
+        'termcolor']
+version = '1.8'
 
 setuptools.setup(
     name='utils-S',
--- utils/__init__.py
+++ utils/__init__.py
@@ -1,2 +1,1 @@
 from . import os_helper, js_helper, request_session, thread_helpers
-version = '1.6.8'
